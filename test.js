569c0bc50c3e0c06e2d7063a49347b0a50d479eb
Vladimir BARGUES <vbargues@e2r11p3.clusters.42paris.fr>
2022-03-19 22:58:49 +0100

    editor and exe list, need linking

--- test.js
+++ test.js
@@ -2,6 +2,14 @@ console.log('#editor');
 console.log($("#editor").text());
 var editor = CodeMirror.fromTextArea(document.getElementById('editor'), {
     mode: "solidity",
+    theme: "solidity",
     lineNumbers: true,
 });
+
 editor.save()
+
+$(document).ready(function() {
+    $("#btnSubmit").click(function(){
+		console.log(editor.getValue());
+    }); 
+});
